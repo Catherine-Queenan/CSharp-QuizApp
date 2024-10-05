72ff86a01c710984a8814dbaae1ee4e0df85e18c
Liam Janicke <liam.jaenicke@gmail.com>
2024-10-05 09:20:00 -0700

    Adjusted Stream API to work with the new media handling

--- WEB-INF/classes/QuizServlet.java
+++ WEB-INF/classes/QuizServlet.java
@@ -19,78 +19,80 @@ public class QuizServlet extends HttpServlet {
         String category = req.getParameter("categoryName");
 
         Connection con = null;
-        Statement stmnt = null;
-        ResultSet rs = null;
-        //StringBuilder quizzesHtml = new StringBuilder();
+        PreparedStatement psQuiz = null;
+        ResultSet rsQuiz = null;
+        PreparedStatement psMedia = null;
+        PreparedStatement psQuizMedia = null;
+        ResultSet rsMedia = null;
+        ResultSet rsQuizMedia = null;
 
         List<Map<String, String>> quizzes = new ArrayList<>();
 
         try {
             Class.forName("com.mysql.cj.jdbc.Driver"); // MySQL Driver
             con = DatabaseUtil.getConnection();
-            stmnt = con.createStatement();
-            rs = stmnt.executeQuery("SELECT name, description FROM quizzes WHERE category_name = \"" + category + "\";");
 
-            while (rs.next()) {
-                //Changes here
+            psQuiz = con.prepareStatement("SELECT name, description FROM quizzes WHERE category_name = ?;");
+            psQuiz.setString(1, category);
+            rsQuiz = psQuiz.executeQuery();
+
+            psMedia = con.prepareStatement("SELECT media_id FROM quiz_media WHERE quiz_name = ?");
+            psQuizMedia = con.prepareStatement("SELECT media_file_path FROM media WHERE id = ?");
+
+            while (rsQuiz.next()) {
+                String quizName = rsQuiz.getString("name");
+                String quizDescription = rsQuiz.getString("description");
+
+                psMedia.setString(1, quizName);
+                rsMedia = psMedia.executeQuery();
+
+                StringBuilder mediaHtml = new StringBuilder();
+                while (rsMedia.next()) {
+                    InputStream mediaId = rsMedia.getBinaryStream("media_id");
+
+                    if (mediaId != null) {
+                        psQuizMedia.setBinaryStream(1, mediaId);
+                        rsQuizMedia = psQuizMedia.executeQuery();
+                        if (rsQuizMedia.next()) {
+                            String mediaFilePath = rsQuizMedia.getString("media_file_path");
+                            mediaHtml.append("<img src=\"").append(mediaFilePath).append("\" alt=\"").append(quizName).append("\" class=\"categoryImg\">");
+                        }
+                    }
+                }
+
                 Map<String, String> quizData = new HashMap<>();
-                quizData.put("name", rs.getString("name"));
-                quizData.put("description", rs.getString("description"));
+                quizData.put("name", quizName);
+                quizData.put("description", quizDescription);
+                quizData.put("mediaHtml", mediaHtml.toString());
                 quizzes.add(quizData);
-
-                //String quizName = rs.getString("name");
-                //String quizDescription = rs.getString("description");
-
-                
-                /*quizzesHtml.append("<div class=\"quiz\">\n")
-                        .append("<form method=\"post\">\n")
-                        .append("    <input type=\"hidden\" name=\"quizName\" value=\"").append(quizName).append("\" />\n")
-                        .append("    <input type=\"submit\" value=\"").append(quizName).append("\" />\n")
-                        // .append("    <label for=\"").append(quizName).append("\">").append(quizName).append("</label>")
-                        .append("<p class=\"quiz-description\">").append(quizDescription).append("</p>\n")
-                        .append("</form>\n");
-                
-                // Show "Add Question" and "Delete Quiz" buttons only for admin users
-                if ("a".equals(role)) {
-                    quizzesHtml.append("<div class=\"adminBtnWrap\">")
-                            // .append("    <button type=\"button\" onclick=\"window.location.href='addQuestion?quizName=")
-                            // .append(quizName).append("'\">Add Question</button>\n")
-                            .append("    <button type=\"button\" onclick=\"window.location.href='deleteQuiz?quizName=")
-                            .append(quizName).append("'\">Delete Quiz</button>\n")
-                            .append("    <button type=\"button\" onclick=\"window.location.href='edit?quizName=")
-                            .append(quizName).append("'\">Edit Quiz</button>\n</div>");
-                }*/
-
-                //quizzesHtml.append("</div>\n");
             }
 
         } catch (Exception e) {
             e.printStackTrace();
         } finally {
-            try {
-                if (rs != null) rs.close();
-            } catch (SQLException e) { e.printStackTrace(); }
-            try {
-                if (stmnt != null) stmnt.close();
-            } catch (SQLException e) { e.printStackTrace(); }
-            try {
-                if (con != null) con.close();
-            } catch (SQLException e) { e.printStackTrace(); }
+            try { if (rsQuiz != null) rsQuiz.close(); } catch (Exception e) { e.printStackTrace(); }
+            try { if (rsMedia != null) rsMedia.close(); } catch (Exception e) { e.printStackTrace(); }
+            try { if (rsQuizMedia != null) rsQuizMedia.close(); } catch (Exception e) { e.printStackTrace(); }
+            try { if (psQuiz != null) psQuiz.close(); } catch (Exception e) { e.printStackTrace(); }
+            try { if (psMedia != null) psMedia.close(); } catch (Exception e) { e.printStackTrace(); }
+            try { if (psQuizMedia != null) psQuizMedia.close(); } catch (Exception e) { e.printStackTrace(); }
+            try { if (con != null) con.close(); } catch (Exception e) { e.printStackTrace(); }
         }
 
-        //Functional Programming
-        //Use Stream API to generate HTML
+        //Application of functional programming: Use Stream API to generate HTML
         String quizzesHtml = quizzes.stream()
             .map(quiz -> {
                 String quizName = quiz.get("name");
                 String quizDescription = quiz.get("description");
-                
+                String mediaHtml = quiz.get("mediaHtml");
+
                 StringBuilder html = new StringBuilder();
                 html.append("<div class=\"quiz\">\n")
                     .append("<form method=\"post\">\n")
                     .append("    <input type=\"hidden\" name=\"quizName\" value=\"").append(quizName).append("\" />\n")
                     .append("    <input type=\"submit\" value=\"").append(quizName).append("\" />\n")
                     .append("<p class=\"quiz-description\">").append(quizDescription).append("</p>\n")
+                    .append("  <div class=\"img\">").append(mediaHtml).append("</div>\n")
                     .append("</form>\n");
 
                 // Show "Add Question" and "Delete Quiz" buttons only for admin users
@@ -111,27 +113,21 @@ public class QuizServlet extends HttpServlet {
         // Forward the request to the quiz.jsp
         RequestDispatcher view = req.getRequestDispatcher("/views/quiz.jsp");
         view.forward(req, res);
-
-
-
-        /*req.setAttribute("quizzesHtml", quizzesHtml.toString());
-        // Forward the request to the quiz.jsp
-        RequestDispatcher view = req.getRequestDispatcher("/views/quiz.jsp");
-        view.forward(req, res);*/
     }
 
     public void doPost(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
         String quizName = req.getParameter("quizName");
 
         Connection con = null;
-        Statement stmnt = null;
+        PreparedStatement ps = null;
         ResultSet rs = null;
 
         try {
             Class.forName("com.mysql.cj.jdbc.Driver"); // MySQL Driver
             con = DatabaseUtil.getConnection();
-            stmnt = con.createStatement();
-            rs = stmnt.executeQuery("SELECT id FROM questions WHERE quiz_name = \"" + quizName + "\" ORDER BY rand();");
+            ps = con.prepareStatement("SELECT id FROM questions WHERE quiz_name = ? ORDER BY rand();");
+            ps.setString(1, quizName);
+            rs = ps.executeQuery();
 
             ArrayList<InputStream> qIDs = new ArrayList<>();
             while (rs.next()) {
@@ -140,7 +136,7 @@ public class QuizServlet extends HttpServlet {
             }
 
             HttpSession session = req.getSession(false);
-            if(session != null){
+            if (session != null) {
                 session.setAttribute("quiz", quizName);
                 session.setAttribute("questions", qIDs);
                 session.setAttribute("currQuestion", 0);
@@ -155,15 +151,9 @@ public class QuizServlet extends HttpServlet {
         } catch (Exception e) {
             e.printStackTrace();
         } finally {
-            try {
-                if (rs != null) rs.close();
-            } catch (SQLException e) { e.printStackTrace(); }
-            try {
-                if (stmnt != null) stmnt.close();
-            } catch (SQLException e) { e.printStackTrace(); }
-            try {
-                if (con != null) con.close();
-            } catch (SQLException e) { e.printStackTrace(); }
+            try { if (rs != null) rs.close(); } catch (SQLException e) { e.printStackTrace(); }
+            try { if (ps != null) ps.close(); } catch (SQLException e) { e.printStackTrace(); }
+            try { if (con != null) con.close(); } catch (SQLException e) { e.printStackTrace(); }
         }
     }
 
@@ -175,9 +165,7 @@ public class QuizServlet extends HttpServlet {
 
         try {
             Class.forName("com.mysql.cj.jdbc.Driver");
-
             con = DatabaseUtil.getConnection();
-            
             String sql = "SELECT role FROM users WHERE username = ?";
             ps = con.prepareStatement(sql);
             ps.setString(1, username);
