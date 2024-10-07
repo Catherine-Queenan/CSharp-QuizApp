b59954d7c870cbe88e54d13cc68ca835baa7d9a3
EugenieKim <eugenie.kim@outlook.com>
2024-10-07 08:28:19 -0700

    Committing to allow a pull

--- WEB-INF/classes/DatabaseUtil.java
+++ WEB-INF/classes/DatabaseUtil.java
@@ -4,7 +4,7 @@ import java.sql.SQLException;
 
 public class DatabaseUtil {
 
-    private static final String URL = "jdbc:mysql://localhost:3306/QuizGame";
+    private static final String URL = "jdbc:mysql://localhost:3306/quizapp";
     private static final String USER = "root";
     private static final String PASSWORD = "";
 
