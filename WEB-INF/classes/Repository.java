af64824dec86a76e036fb5359012deec9a7f42f2
Catherine Queenan <catherinerqueenan@gmail.com>
2024-10-07 09:44:17 -0700

    Reset to pull again

--- WEB-INF/classes/Repository.java
+++ WEB-INF/classes/Repository.java
@@ -13,7 +13,7 @@ public class Repository implements IRepository {
     public static final String URL = "jdbc:mysql://localhost:3306/QuizApp";
     public static final String USER = "root";
     public static final String PASSWORD = "Cathgirlh6*";
-    private Connection con = null;
+    private Connection con = null;   
 
     private void insertCategory(JSONObject entry) {
         try {
